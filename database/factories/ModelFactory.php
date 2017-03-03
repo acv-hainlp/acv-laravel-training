ca1670698f6f9a0fa2843948d6278f802d4cca1c
Hai Nguyen <acv-hainlp@atmarkcafe.org>
2017-03-03 17:54:39 +0700

    factory and testing

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,4 +21,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
-});
+}); //howtouse: tinker: factory('App\User',{number})->create(); /make();
+
+ 
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body'	=> $faker->paragraph,
+        'user_id' => factory(App\User::class)->create()->id,
+    ];
+}); //howtouse: tinker: factory('App\User',{number})->create(); /make();
+
+ 
